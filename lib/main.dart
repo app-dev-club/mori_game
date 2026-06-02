a27c72b75f49330c4bbc75d90b4b1c1b07adccee
hirotakasuzuki1219 <hirotaka.suzuki.1219@gmail.com>
2026-06-02 21:48:00 +0900

    authをインポートしない

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart'; 
+// import 'package:firebase_auth/firebase_auth.dart'; 
 import 'package:mori_game/features/entrance/entrance_page.dart';
 import 'firebase_options.dart';
 
